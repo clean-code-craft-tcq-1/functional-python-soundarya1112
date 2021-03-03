22933100a0d7f86b26851409617c38bca832e23e
soundarya1112 <74286544+soundarya1112@users.noreply.github.com>
2021-03-03 20:20:40 +0530

    Updated the Tasks accordingly

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,32 @@
+battery_allowedValues = {
+        'temperature'      : {'min': 0, 'max': 45},
+        'state_of_charge'  : {'min': 20, 'max': 80},
+        'charge_rate'      : {'min': 0,'max': 0.8}
+            } 
+def battery_validationcheck(battery_inputs):
+        outOfIndex_Values = []
+        for bmsParam_name,bmsParam_value in battery_inputs.items() :
+            battery_Limit_Check(bmsParam_name,bmsParam_value,outOfIndex_Values)
+            
+        return outOfIndex_Values
 
-def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
+    
+    def battery_Limit_Check(bmsParam_name,bmsParam_value): 
+        if (bmsParam_value < battery_allowedValues[bmsParam_name]['min']) or (bmsParam_value > battery_allowedValues[bmsParam_name]['max']):
+            outOfIndex_Values.append(bmsParam_name)
+            
+        
+    def battery_is_ok(battery_inputs):
+        battery_range_check = battery_validationcheck(battery_inputs)
+            if len(battery_range_check) == 0
+                return True
+            else :
+                return False
 
-  return True
+    if __name__ == '__main__':
+        assert(battery_is_ok({'temperature': 15,'Soc': 65, 'Charge_rate': 0.6}) is True) 
+        assert(battery_is_ok({'temperature': 70,'Soc': 90, 'Charge_rate': 0.9}) is False)  
+        assert(battery_is_ok({'temperature': 46,'Soc': 10, 'Charge_rate': 0.9}) is False)
 
 
-if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+
